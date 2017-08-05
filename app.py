440a92b1e55f7f1fc4d3f6b25e292fd6dec71316
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-08-05 16:01:05 +0800

    normal update
    add news spider to provider news push service.

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from flask_sockets import Sockets
 
 from views.todos import todos_view
 
+import os
+import sys
+import urllib2
+import requests
+import re
+from lxml import etree
+
 app = Flask(__name__)
 
 # 动态路由
@@ -25,4 +32,51 @@ def time():
 
 @app.route('/news')
 def news():
-    return str("news")
+    start_url = "http://news.163.com/rank/"
+    Spider(start_url)
+
+
+def StringListSave(slist):
+    for s in slist:
+    	fp.write("%s\n" % (s[0].encode("utf8")))
+
+def Page_Info(myPage):
+    '''Regex'''
+    mypage_Info = re.findall(r'<div class="titleBar" id=".*?"><h2>(.*?)</h2><div class="more"><a href="(.*?)">.*?</a></div></div>', myPage, re.S)
+    return mypage_Info
+
+def New_Page_Info(new_page):
+    '''Regex(slowly) or Xpath(fast)'''
+    # new_page_Info = re.findall(r'<td class=".*?">.*?<a href="(.*?)\.html".*?>(.*?)</a></td>', new_page, re.S)
+    # # new_page_Info = re.findall(r'<td class=".*?">.*?<a href="(.*?)">(.*?)</a></td>', new_page, re.S) # bugs
+    # results = []
+    # for url, item in new_page_Info:
+    #     results.append((item, url+".html"))
+    # return results
+    dom = etree.HTML(new_page)
+    new_items = dom.xpath('//tr/td/a/text()')
+    new_urls = dom.xpath('//tr/td/a/@href')
+    assert(len(new_items) == len(new_urls))
+    return zip(new_items, new_urls)
+
+def Spider(url):
+    i = 0
+    print "downloading ", url
+    myPage = requests.get(url).content.decode("gbk")
+    # myPage = urllib2.urlopen(url).read().decode("gbk")
+    myPageResults = Page_Info(myPage)
+    StringListSave(myPageResults)
+    i += 1
+    for item, url in myPageResults:
+        print "downloading ", url
+        new_page = requests.get(url).content.decode("gbk")
+        # new_page = urllib2.urlopen(url).read().decode("gbk")
+        newPageResults = New_Page_Info(new_page)
+        filename = str(i)+"_"+item
+        StringListSave(newPageResults)
+        i += 1
+
+
+
+
+

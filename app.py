9f4917ff3a3f914ac972775df9c0bb65c73a35bd
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-08-05 17:34:20 +0800

    normal update
    see if iteration works.

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def news():
     url = "http://news.163.com/rank/"
     myPage = requests.get(url).content.decode("gbk")
     myPageResults =  re.findall(r'<div class="titleBar" id=".*?"><h2>(.*?)</h2><div class="more"><a href="(.*?)">.*?</a></div></div>', myPage, re.S)
+    news = []
     for item, url in myPageResults:
         new_page = requests.get(url).content.decode("gbk")
         dom = etree.HTML(new_page)
@@ -42,6 +43,7 @@ def news():
         new_urls = dom.xpath('//tr/td/a/@href')
         for item in new_items:
             item = item.encode("utf-8")
-    return str("hello news")
+            news.append(item)
+    return render_template('index.html',news=news)
 
 

42b59e27ee91cd0a8469d7c4c547f1a6cfa9e663
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2018-11-29 21:04:32 +0800

    fix return
    1.NU

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def qiniu():
 		bucket_name = 'file'
 		token = q.upload_token(bucket_name, key, 3600)
 		ret, info = put_file(token, key, './'+key)
-		return ret
+		return str(datetime.now())
 
 
 @app.route('/words')

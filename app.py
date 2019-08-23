1670d41b9af0771021b63051bc3440347d460817
deepOceanOne <lifeinbofyx@sina.com>
2019-08-23 22:36:11 +0800

    TS
    TS

--- app.py
+++ app.py
@@ -114,8 +114,15 @@ def qiniu():
 @app.route('/postwrite',methods=['GET','POST'])   # 接收从倍洽外部post过来的消息，触发词为“POST”
 def postwrite():
 	if request.method == 'POST' :
-		data = request.get_data()
-		data = json.loads(data)
+		if request.content_type.startswith('application/json'):
+	        data = request.get_data()
+	        data = json.loads(data)
+	    else:
+	        for key, value in request.form.items():
+	            if key.endswith('[]'):
+	                data[key[:-2]] = request.form.getlist(key)
+	            else:
+	                data[key] = value
 		text_content = data['content']
 		Writings = leancloud.Object.extend('Writings')
 		writing = Writings()
